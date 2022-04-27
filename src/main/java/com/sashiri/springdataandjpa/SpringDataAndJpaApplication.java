95d14689b9bbd7d82a46bb75f13f981536087e26
Diego Marques <marquesashiri@gmail.com>
2022-04-27 15:45:39 -0300

    first commit

---
+++ src/main/java/com/sashiri/springdataandjpa/SpringDataAndJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sashiri.springdataandjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAndJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataAndJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sashiri/springdataandjpa/SpringDataAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashiri.springdataandjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAndJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
